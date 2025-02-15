6d04450f4416ec97a564675b786cd86231a99ed1
nemesisverse <129728622+nemesisverse@users.noreply.github.com>
2025-02-15 15:28:40 +0530

    Projectile setup 1

--- Assets/Scripts/ProjectileLaunceher.cs
+++ Assets/Scripts/ProjectileLaunceher.cs
@@ -1,6 +1,8 @@
+using System;
+using Unity.Netcode;
 using UnityEngine;
 
-public class ProjectileLaunceher : MonoBehaviour
+public class ProjectileLaunceher : NetworkBehaviour  
 {
     [Header("References")]
     [SerializeField] private InputReader inputReader;
@@ -10,9 +12,44 @@ public class ProjectileLaunceher : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField] private float projectileSpeed;
+
+    public bool shouldFire;
+    public override void OnNetworkSpawn()
+    {
+        if(!IsOwner) { return; }
+        inputReader.PrimaryFireEvent += HandlePrimaryFire;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if(!IsOwner) { return; }
+        inputReader.PrimaryFireEvent -= HandlePrimaryFire;
+
+    }
+
+    public void HandlePrimaryFire(bool shouldFire)
+    {
+        this.shouldFire = shouldFire;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if(!IsOwner) { return; }
+        if (!shouldFire) {  return; }
+        SpawnDummyProjectile(projectileSpawnPoint.position, projectileSpawnPoint.up);
+    }
+
+    [ServerRpc]
+
+    private void PrimaryFireServerRpc(Vector3 spawnPos, Vector3 direction)
+    {
+        GameObject projectileInstance = Instantiate(clientProjectilePrefab, spawnPos, Quaternion.identity);
+        projectileInstance.transform.up = direction;
+    }
+
+    private void SpawnDummyProjectile(Vector3 spawnPos, Vector3 direction)
+    {
+       GameObject projectileInstance =  Instantiate(clientProjectilePrefab  , spawnPos , Quaternion.identity);
+        projectileInstance.transform.up = direction;
     }
 }
